7b6d40cf25a10b72f00dbdc08e0b6bcaded795da
Arda Koç <ardaskoc@gmail.com>
2023-12-27 16:23:20 +0300

    created marketing page folder. updated the content of the marketing page.

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,17 @@
+const MarketingLayout = ({
+    children
+}: {
+    children: React.ReactNode
+}) => {
+    return (
+        <div className="h-full bg-slate-100">
+            {/* TODO: Navbar */}
+            <main className="pt-40 pb-20 bg-slate-100">
+                {children}
+            </main>
+            {/* TODO:Footer */}
+        </div>
+    )
+}
+
+export default MarketingLayout;
