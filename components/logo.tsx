96c8ff29188045f45ea41f1902451808bd715b3a
Arda Koç <ardaskoc@gmail.com>
2023-12-30 16:39:05 +0300

    added app logo.

---
+++ components/logo.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image"
+import Link from "next/link"
+import localFont from "next/font/local"
+
+import { cn } from "@/lib/utils"
+
+const headingFont = localFont({
+    src: "../public/fonts/font.woff2",
+})
+
+export const Logo = () => {
+    return (
+        <Link href="/">
+            <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+                <Image
+                    src="/images/logo.svg"
+                    alt="Logo"
+                    height={28}
+                    width={28}
+                />
+                <p className={cn(
+                    "text-lg text-neutral-700 pt-1",
+                    headingFont.className,
+                )}>
+                    Taskello
+                </p>
+            </div>
+        </Link>
+    )
+}
