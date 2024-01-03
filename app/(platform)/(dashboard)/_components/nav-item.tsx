a3ccf4d61656c3cf31b1a5d409850e1f9bebb4df
Arda Koç <ardaskoc@gmail.com>
2024-01-03 20:24:25 +0300

    added accordion items to under the sidebar nav items.

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,10 +1,13 @@
 "use client"
 
 import Image from "next/image"
+import { usePathname, useRouter } from "next/navigation"
+import { Activity, CreditCard, Layout, Settings } from "lucide-react"
 
 
 import { cn } from "@/lib/utils"
-import { AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
+import { AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
+import { Button } from "@/components/ui/button"
 
 export type Organization = {
     id: string
@@ -26,6 +29,35 @@ export const NavItem = ({
     organization,
     onExpand
 }: NavItemProps) => {
+    const router = useRouter()
+    const pathname = usePathname()
+    const routes = [
+        {
+            label: "Boards",
+            icon: <Layout className="h-4 w-4 mr-2" />,
+            href: `/organization/${organization.id}`,
+        },
+        {
+            label: "Activity",
+            icon: <Activity className="h-4 w-4 mr-2" />,
+            href: `/organization/${organization.id}/activity`,
+        },
+        {
+            label: "Settings",
+            icon: <Settings className="h-4 w-4 mr-2" />,
+            href: `/organization/${organization.id}/settings`,
+        },
+        {
+            label: "Billing",
+            icon: <CreditCard className="h-4 w-4 mr-2" />,
+            href: `/organization/${organization.id}/billing`,
+        },
+    ]
+
+    const onClick = (href: string) => {
+        router.push(href)
+    }
+
     return (
         <AccordionItem
             value={organization.id}
@@ -52,6 +84,23 @@ export const NavItem = ({
                     </span>
                 </div>
             </AccordionTrigger>
+            <AccordionContent className="pt-1 text-neutral-700">
+                {routes.map((route) => (
+                    <Button
+                        key={route.href}
+                        size="sm"
+                        onClick={() => onClick(route.href)}
+                        variant="ghost"
+                        className={cn(
+                            "w-full font-normal justify-start pl-10 mb-1",
+                            pathname === route.href && "bg-sky-500/10 text-sky-700"
+                        )}
+                    >
+                        {route.icon}
+                        {route.label}
+                    </Button>
+                ))}
+            </AccordionContent>
         </AccordionItem>
     )
 }
