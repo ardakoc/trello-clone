677e708fc24fdf533e36fc66db5d326fdfffded7
Arda Koç <ardaskoc@gmail.com>
2024-01-14 11:50:25 +0300

    updated form popover component.

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -1,10 +1,16 @@
 "use client"
 
+import { X } from "lucide-react"
 import {
     Popover,
+    PopoverClose,
     PopoverContent,
     PopoverTrigger
 } from "@/components/ui/popover"
+import { Button } from "@/components/ui/button"
+
+import { FormInput } from "./form-input"
+import { FormSubmit } from "./form-submit"
 
 interface FormPopoverProps {    
     children: React.ReactNode
@@ -33,6 +39,26 @@ export const FormPopover = ({
                 <div className="text-sm font-medium text-center text-neutral-600 pb-4">
                     Create board
                 </div>
+                <PopoverClose asChild>
+                    <Button
+                        className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+                        variant="ghost"
+                    >
+                        <X className="h-4 w-4" />
+                    </Button>
+                </PopoverClose>
+                <form className="space-y-4">
+                    <div className="space-y-4">
+                        <FormInput
+                            id="title"
+                            label="Board title"
+                            type="text"
+                        />
+                    </div>
+                    <FormSubmit className="w-full">
+                        Create
+                    </FormSubmit>
+                </form>
             </PopoverContent>
         </Popover>
     )
