a41d9a48367d8fb34b187613017a6d1d8a683faa
Arda Koç <ardaskoc@gmail.com>
2024-01-16 00:32:04 +0300

    added links of image owners to the bottom of the images on hover.

--- components/form/form-picker.tsx
+++ components/form/form-picker.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Image from "next/image"
+import Link from "next/link"
 import { Loader2 } from "lucide-react"
 import { useFormStatus } from "react-dom"
 import { useEffect, useState } from "react"
@@ -78,6 +79,13 @@ export const FormPicker = ({
                             className="object-cover rounded-sm"
                             fill
                         />
+                        <Link
+                            href={image.links.html}
+                            target="_blank"
+                            className="opacity-0 group-hover:opacity-100 absolute bottom-0 w-full text-[10px] truncate text-white hover:underline p-1 bg-black/50"
+                        >
+                            {image.user.name}
+                        </Link>
                     </div>
                 ))}
             </div>
