d0905fa8a40f1494d2105af530677bf783eed9af
Arda Koç <ardaskoc@gmail.com>
2024-01-19 15:38:23 +0300

    updated organization page title.

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,15 @@
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs";
+
 import { OrganizationControl } from "./_components/organization-control";
+
+export async function generateMetadata() {
+    const { orgSlug } = auth()
+
+    return {
+        title: startCase(orgSlug || "organization"),
+    }
+}
 
 const OrganizationIdLayout = ({
     children
