56a286ad4fa2afa10d329011c866bf02777262d1
Arda Koç <ardaskoc@gmail.com>
2024-01-30 14:02:54 +0300

    updated board title update action.

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -1,10 +1,13 @@
 "use client"
 
+import { toast } from "sonner"
 import { ElementRef, useRef, useState } from "react"
 import { Board } from "@prisma/client"
 
 import { Button } from "@/components/ui/button"
 import { FormInput } from "@/components/form/form-input"
+import { updateBoard } from "@/actions/update-board"
+import { useAction } from "@/hooks/use-action"
 
 interface BoardTitleFormProps {
     data: Board
@@ -13,9 +16,21 @@ interface BoardTitleFormProps {
 export const BoardTitleForm = ({
     data
 }: BoardTitleFormProps) => {
+    const { execute } = useAction(updateBoard, {
+        onSuccess: (data) => {
+            toast.success(`Board "${data.title}" updated!`)
+            setTitle(data.title)
+            disableEditing()
+        },
+        onError: (error) => {
+            toast.error(error)
+        }
+    })
+
     const formRef = useRef<ElementRef<"form">>(null)
     const inputRef = useRef<ElementRef<"input">>(null)
 
+    const [title, setTitle] = useState(data.title)
     const [isEditing, setIsEditing] = useState(false)
 
     const enableEditing = () => {
@@ -32,7 +47,11 @@ export const BoardTitleForm = ({
 
     const onSubnit = (formData: FormData) => {
         const title = formData.get("title") as string
-        console.log("Submitted", title)
+        
+        execute({
+            title,
+            id: data.id,
+        })
     }
 
     const onBlur = () => {
@@ -46,7 +65,7 @@ export const BoardTitleForm = ({
                     id="title"
                     ref={inputRef}
                     onBlur={onBlur}
-                    defaultValue={data.title}
+                    defaultValue={title}
                     className="text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent border-none"
                 />
             </form>
@@ -59,7 +78,7 @@ export const BoardTitleForm = ({
             className="font-bold text-lg h-auto w-auto p-1 px-2"
             onClick={enableEditing}
         >
-            {data.title}
+            {title}
         </Button>
     )
 }
