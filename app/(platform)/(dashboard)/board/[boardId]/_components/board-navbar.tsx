b90445d0318b4462e1dfc408e1b58a5e8dce50cf
Arda Koç <ardaskoc@gmail.com>
2024-01-30 14:17:49 +0300

    added board options component.

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,6 +1,7 @@
 import { Board } from "@prisma/client"
 
 import { BoardTitleForm } from "./board-title-form"
+import { BoardOptions } from "./board-options"
 
 interface BoardNavbarProps {
     data: Board
@@ -12,6 +13,9 @@ export const BoardNavbar = async ({
     return (
         <div className="fixed top-14 w-full h-14 z-[40] bg-black/50 flex items-center px-6 gap-x-4 text-white">
             <BoardTitleForm data={data} />
+            <div className="ml-auto">
+                <BoardOptions id={data.id} />
+            </div>
         </div>
     )
 }
---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-options.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { MoreHorizontal, X } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+    Popover,
+    PopoverClose,
+    PopoverContent,
+    PopoverTrigger
+} from "@/components/ui/popover"
+
+interface BoardOptionsProps {
+    id: string
+}
+
+export const BoardOptions = ({
+    id
+}: BoardOptionsProps) => {
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button variant="transparent" className="h-auto w-auto p-2">
+                    <MoreHorizontal className="h-4 w-4" />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent
+                className="px-0 pt-3 pb-3"
+                side="bottom"
+                align="start"
+            >
+                <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+                    Board actions
+                </div>
+                <PopoverClose asChild>
+                    <Button
+                        variant="ghost"
+                        className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+                    >
+                        <X className="h-4 w-4" />
+                    </Button>
+                </PopoverClose>
+                <Button
+                    variant="ghost"
+                    onClick={() => {}}
+                    className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+                >
+                    Delete this board
+                </Button>
+            </PopoverContent>
+        </Popover>
+    )
+}
