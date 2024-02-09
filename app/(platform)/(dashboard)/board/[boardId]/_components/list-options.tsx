18476f78ef9f117e09aabc52af3511820aff2517
Arda Koç <ardaskoc@gmail.com>
2024-02-09 14:10:13 +0300

    added a server action for deleting the list.

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { ElementRef, useRef } from "react"
+import { toast } from "sonner"
 import { MoreHorizontal, X } from "lucide-react"
 import { List } from "@prisma/client"
 
@@ -9,8 +11,10 @@ import {
     PopoverContent,
     PopoverTrigger
 } from "@/components/ui/popover"
-import { Button } from "@/components/ui/button"
+import { useAction } from "@/hooks/use-action"
+import { deleteList } from "@/actions/delete-list"
 import { FormSubmit } from "@/components/form/form-submit"
+import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 
 interface ListOptionsProps {
@@ -22,6 +26,25 @@ export const ListOptions = ({
     data,
     onAddCard
 }: ListOptionsProps) => {
+    const closeRef = useRef<ElementRef<"button">>(null)
+
+    const { execute: executeDelete } = useAction(deleteList, {
+        onSuccess: (data) => {
+            toast.success(`List "${data.title}" deleted`)
+            closeRef.current?.click()
+        },
+        onError: (error) => {
+            toast.error(error)
+        },
+    })
+
+    const onDelete = (formData: FormData) => {
+        const id = formData.get("id") as string
+        const boardId = formData.get("boardId") as string
+
+        executeDelete({id, boardId})
+    }
+
     return (
         <Popover>
             <PopoverTrigger asChild>
@@ -40,7 +63,7 @@ export const ListOptions = ({
                 <div className="text-sm font-medium text-center text-neutral-600 pb-4">
                     List actions
                 </div>
-                <PopoverClose asChild>
+                <PopoverClose ref={closeRef} asChild>
                     <Button
                         variant="ghost"
                         className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
@@ -66,7 +89,9 @@ export const ListOptions = ({
                     </FormSubmit>
                 </form>
                 <Separator />
-                <form>
+                <form
+                    action={onDelete}
+                >
                     <input hidden name="id" id="id" value={data.id}/>
                     <input hidden name="boardId" id="boardId" value={data.boardId}/>
                     <FormSubmit
