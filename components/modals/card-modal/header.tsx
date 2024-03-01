8b80977d78795b6d6edcc968d7b1871c7ee19850
Arda Koç <ardaskoc@gmail.com>
2024-03-01 13:22:20 +0300

    added update card action to the card modal.

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -1,11 +1,14 @@
 "use client"
 
+import { toast } from "sonner"
 import { ElementRef, useRef, useState } from "react"
 import { Layout } from "lucide-react"
 import { useParams } from "next/navigation"
 import { useQueryClient } from "@tanstack/react-query"
 
 import { CardWithList } from "@/types"
+import { useAction } from "@/hooks/use-action"
+import { updateCard } from "@/actions/update-card"
 import { FormInput } from "@/components/form/form-input"
 import { Skeleton } from "@/components/ui/skeleton"
 
@@ -19,6 +22,20 @@ export const Header = ({
     const queryClient = useQueryClient()
     const params = useParams()
 
+    const { execute } = useAction(updateCard, {
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({
+                queryKey: ["card", data.id]
+            })
+
+            toast.success(`Renamed to "${data.title}"`)
+            setTitle(data.title)
+        },
+        onError: (error) => {
+            toast.error(error)
+        }
+    })
+
     const inputRef = useRef<ElementRef<"input">>(null)
 
     const [title, setTitle] = useState(data.title)
@@ -28,7 +45,18 @@ export const Header = ({
     }
 
     const onSubmit = (formData: FormData) => {
-        console.log(formData.get("title"))
+        const title = formData.get("title") as string
+        const boardId = params.boardId as string
+
+        if (title === data.title) {
+            return
+        }
+
+        execute({
+            title,
+            id: data.id,
+            boardId,
+        })
     }
 
     return (
