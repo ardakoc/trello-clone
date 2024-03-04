b21276dd9d024c9ba78df7c27902480c3e809ba5
Arda Koç <ardaskoc@gmail.com>
2024-03-04 16:15:39 +0300

    added create audit log method to all actions.

--- actions/copy-card/index.ts
+++ actions/copy-card/index.ts
@@ -3,9 +3,11 @@
 import { auth } from "@clerk/nextjs"
 import { revalidatePath } from "next/cache"
 import { redirect } from "next/navigation"
+import { ACTION, ENTITY_TYPE } from "@prisma/client"
 
 import { db } from "@/lib/db"
 import { createSafeAction } from "@/lib/create-safe-action"
+import { createAuditLog } from "@/lib/create-audit-log"
 
 import { InputType, ReturnType } from "./types"
 import { CopyCard } from "./schema"
@@ -53,6 +55,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
                 description: cardToCopy.description,
                 order: newOrder,
             },
+        })
+
+        await createAuditLog({
+            entityId: card.id,
+            entityTitle: card.title,
+            entityType: ENTITY_TYPE.CARD,
+            action: ACTION.CREATE,
         })
     } catch(error) {
         return {
