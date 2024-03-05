8d5e45807198ad9bfa23f0246c902baba3c04d6c
Arda Koç <ardaskoc@gmail.com>
2024-03-05 15:05:29 +0300

    added activity list component to the activity page.

--- app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -1,12 +1,19 @@
+import { Suspense } from "react";
+
 import { Separator } from "@/components/ui/separator";
+
 import { Info } from "../_components/info";
+
+import { ActivityList } from "./_components/activity-list";
 
 const ActivityPage = () => {
     return (
         <div className="w-full">
             <Info />
             <Separator className="my-4" />
-            <p>Activity List</p>
+            <Suspense fallback={<ActivityList.Skeleton />}>
+                <ActivityList />
+            </Suspense>
         </div>
     )
 }
