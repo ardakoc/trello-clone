af0f4fa3fb616f048c4b5370dfd8e2cc0623b93e
Arda Koç <ardaskoc@gmail.com>
2024-03-06 11:35:24 +0300

    updated remaining boards text with subscription check.

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -4,11 +4,12 @@ import { redirect } from "next/navigation"
 import Link from "next/link"
 
 import { db } from "@/lib/db"
+import { getAvailableCount } from "@/lib/org-limit"
+import { checkSubscription } from "@/lib/subscription"
 import { Hint } from "@/components/hint"
 import { FormPopover } from "@/components/form/form-popover"
 import { Skeleton } from "@/components/ui/skeleton"
 import { MAX_FREE_BOARDS } from "@/constants/boards"
-import { getAvailableCount } from "@/lib/org-limit"
 
 export const BoardList = async () => {
     const { orgId } = auth()
@@ -27,6 +28,7 @@ export const BoardList = async () => {
     })
 
     const availableCount = await getAvailableCount()
+    const isPro = await checkSubscription()
 
     return (
         <div className="space-y-4">
@@ -55,7 +57,7 @@ export const BoardList = async () => {
                     >
                         <p className="text-sm">Create new board</p>
                         <span className="text-xs">
-                            {MAX_FREE_BOARDS - availableCount} remaining
+                            {isPro ? "Unlimited" : `${MAX_FREE_BOARDS - availableCount} remaining`}
                         </span>
                         <Hint
                             sideOffset={40}
